1508dc17aa071332c7707d3d07bb62f87c21dca3
khiem <khiemnguyen.hye@gmail.com>
2025-09-27 13:40:44 +0700

    Add clarifying comments

--- interceptron/content.js
+++ interceptron/content.js
@@ -1,7 +1,7 @@
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('injected-script.js');
-document.documentElement.appendChild(script);
-
-script.onload = function() {
-    this.remove();
+const script = document.createElement('script'); // Creates a script DOM element
+script.src = chrome.runtime.getURL('injected-script.js'); // Gets the URL to the script
+document.documentElement.appendChild(script); // Append the <script> element to the root of the <html>
+// <--- This causes the page to load and execute the script in its own context
+script.onload = function() { // Add a listener for when the script is loaded and remove it from the DOM
+    this.remove(); // This remove the 'node', the script that overwrote window.fetch remained
 };
--- interceptron/injected-script.js
+++ interceptron/injected-script.js
@@ -1,19 +1,21 @@
 (function() {
+  // window.fetch is a original function of Chrome, we save it here - just in case.
   const originalFetch = window.fetch;
 
-  window.fetch = async function(...args) {
+  window.fetch = async function(...args) { //  This replaces the ORIGINAL with our script
+    // If the first arg is a string ie fetch('/api', ...), print it
+    // If the first arg is a fetch(new Request(...), ...), gets its URL
     const requestUrl = typeof args[0] === 'string' ? args[0] : args[0].url;
-
+    // Log the URL
     console.log("Intercepting a Fetch request to:", requestUrl);
-
-    const response = await originalFetch(...args);
-    const clonedResponse = response.clone();
+    const response = await originalFetch(...args); // Get the response of the ORIGINAL FETCH
+    const clonedResponse = response.clone(); // Clone the res - its a stream, we need to clone it to not mess it up
     clonedResponse.text().then(text => {
       const contentPreview = text.length > 200 ? text.substring(0, 200) + "..." : text;
       console.log("Response from", requestUrl, " | Status:", response.status, " | Preview:", contentPreview);
     }).catch(e => {
       console.error("Error reading response body for:", requestUrl, e);
     });
-    return response;
+    return response; // Returns the original response in order not to break the page
   };
 })();
