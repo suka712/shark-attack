7486150df73be59aba3c58074cf05e2d165409f0
khiem <khiemnguyen.hye@gmail.com>
2025-10-03 13:46:03 +0700

    Add config.js to store sheetID

--- whistler/background.js
+++ whistler/background.js
@@ -1,3 +1,5 @@
+import { CONFIG } from "./config.js";
+
 chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
   if (message.type === "WRITE_TO_SHEET") {
     writeToSheet(message.data);
@@ -5,17 +7,13 @@ chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
 });
 
 async function writeToSheet(paymentInfo) {
-  const SPREADSHEET_ID = "1bqJmP1S4jOqEfbqQ3pC1F72FiQM9Cpa43hs3pdGuXm8";
-  const APPEND_RANGE = "Sheet1!A1";
-
   chrome.identity.getAuthToken({ interactive: true }, async (token) => {
     if (chrome.runtime.lastError) {
       console.error("Auth error:", chrome.runtime.lastError.message);
       return;
     }
-
     try {
-      const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${APPEND_RANGE}:append?valueInputOption=USER_ENTERED`;
+      const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${CONFIG.sheetId}/values/Sheet1!A1:append?valueInputOption=USER_ENTERED`;
 
       const content = {
         values: [[paymentInfo.responseTime, paymentInfo.msg]],
---
+++ whistler/config.js
@@ -0,0 +1,3 @@
+export const CONFIG = Object.freeze({
+  sheetId: "1bqJmP1S4jOqEfbqQ3pC1F72FiQM9Cpa43hs3pdGuXm8",
+})
